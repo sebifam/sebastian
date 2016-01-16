1de111b8532a2772228104029ce6f59b74fdfc01
sebifam <sebienea2@gmail.com>
2016-01-16 00:40:26 -0800

    Am adaugat niste fisiere

--- angular-js-app/js/app.js
+++ angular-js-app/js/app.js
@@ -1,7 +1,49 @@
 var app = angular.module('blog', [ ]);
 
-app.controller('HomeController', ['$scope', function($scope) {
+app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
   $scope.helloWorld = 'Aplicatii Web cu suport Java!';
-  $scope.myName = 'Example variable.';
 
+  $scope.persoane = [];
+  $scope.keys = [];
+
+  $scope.person = {};
+  $scope.editPerson = {};
+
+
+
+  $http.get('http://localhost:8080/persoana').then(
+    function successCallback(response) {
+
+    $scope.persoane = response;
+    $scope.keys = Object.keys(response.data[0]);
+  });
+
+
+  $scope.addPersoana = function(person) {
+    $scope.persoane.data.push(person);
+    $http.post('http://localhost:8080/persoana', person);
+    $scope.person = {};
+  };
+
+  $scope.setUpdatePerson = function(person) {
+    $scope.editPerson = person;
+  };
+
+  $scope.updatePerson = function() {
+    $http.put('http://localhost:8080/persoana', $scope.editPerson);
+    $scope.editPerson = {};
+  };
+
+  $scope.deletePersoana = function(id) {
+    $http.delete('http://localhost:8080/persoana/' + id)
+    .then(
+      function successCallback(response) {
+        /* code goes here */
+      },
+      function errorCallback(response) {
+        angular.element('[data-id=' + id + ']').remove();
+    });
+  };
+
+  $scope.myArray = ['Elem 1', 'Elem 2', 'Elem 3', 'Elem 4'];
 }]);
---
+++ spring-java-app/src/main/java/hello/models/Persoana.java
@@ -0,0 +1,24 @@
+package hello;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Persoana {
+  private String name;
+  private int id;
+
+  public Persoana() {}
+
+  public Persoana(int id, String name) {
+      this.name = name;
+      this.id = id;
+  }
+
+  public String getName() {
+      return this.name;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+}
